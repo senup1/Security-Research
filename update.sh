310562efa9d556d4a2eb1cd13a79617420b144f4
senup1 <sumanth_353@yahoo.com>
2026-05-03 23:41:10 -0400

    Changing repo structure to use github pages

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run this from ~/myStuff/myCyberSec after exporting from CherryTree
+# Usage: ./update.sh
+
+EXPORT_DIR="CyberSecNotes.ctb_HTML"
+
+if [ ! -d "$EXPORT_DIR" ]; then
+    echo "Export folder not found. Make sure you exported from CherryTree first."
+    exit 1
+fi
+
+read -p "Commit message: " COMMIT_MSG
+
+if [ -z "$COMMIT_MSG" ]; then
+    echo "No commit message entered. Aborting."
+    exit 1
+fi
+
+echo "Clearing old docs..."
+rm -rf docs/*
+
+echo "Moving new files to docs..."
+mv "$EXPORT_DIR"/* docs/
+rmdir "$EXPORT_DIR"
+
+echo "Committing to GitHub..."
+git add .
+git commit -m "$COMMIT_MSG"
+git push
+
+echo "Done."
